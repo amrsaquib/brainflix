7f875f3386977c6003588f4a00326041cc1a7a84
amrsaquib <amrsaquib@gmail.com>
2023-06-20 17:20:36 -0400

    finished header minus some styling discrepancies which ill in the end

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,23 @@ import logo from "../../assets/logo/BrainFlix-logo.svg"
 import search from "../../assets/icons/search.svg"
 import Button from "../Button/Button"
 import upload from "../../assets/icons/upload.svg"
+import pfp from "../../assets/images/Mohan-muruge.jpg"
+import "./Header.scss"
 
 function Header() {
     return (
-        <header>
-            <img src={logo} />
-            <div>
-                <label> <img src={search} /> <input type="text"></input></label>
-                <Button imgsrc={upload} txt="UPLOAD" />
-                <div></div>
+        <header className="header">
+            <img className="header__logo" src={logo} />
+            <div className="header__functionality">
+                <div className="header__mobile-search"> 
+                    <label className="header__searchbar">
+                        <img src={search} />
+                        <input type="text" placeholder="Search"></input>
+                    </label>
+                    <img className="header__pfp header__pfp--mobile" src={pfp}/>
+                </div>
+                <Button imgsrc={upload} txt="UPLOAD" className="header__button"/>
+                <img className="header__pfp header__pfp--desktop" src={pfp}/>
             </div>
         </header>
     )
